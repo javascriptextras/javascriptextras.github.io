c3d8737ca0009d3effb648812d35a0f82ac738da
mimmeer <175632698+mimmeer-github@users.noreply.github.com>
2024-11-06 20:14:27 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
+function buttonClick() {
+  addHTML("body", "<p>You may look at the code: <a href=\"https://github.com/javascriptextras/javascriptextras/blob/main/jsx/\">Github</a>");
+}
+
 import { ver } from "https://javascriptextras.github.io/javascriptextras/jsx/main.js";
 import { decimalAdd, decimalSub, decimalMul, decimalDiv } from "https://javascriptextras.github.io/javascriptextras/jsx/math.js";
 import { replaceHTML, replaceATTRIB, addHTML } from "https://javascriptextras.github.io/javascriptextras/jsx/dom.js";
+import { createButton, createLabel, createDiv } from "https://javascriptextras.github.io/javascriptextras/jsx/ui.js";
 console.log("JSX Version " + ver());
 alert(decimalAdd(1, 0.4));
 alert(decimalSub(0.3, 0.2));
@@ -9,3 +14,4 @@ alert(decimalDiv(0.3, 0.2));
 replaceHTML("tst", "Yeah!");
 replaceATTRIB("tst", "style", "color: red");
 addHTML("body", '<p>Yeah</p>');
+createButton("Show Code", "buttonClick();");
