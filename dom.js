3f788c241acd04a6f4546eb7f8ac0bf63ee1beb5
mimmeer <175632698+mimmeer-github@users.noreply.github.com>
2024-11-06 16:14:30 +0000

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,7 @@
+export function replaceHTML(ID, INNER) {
+  document.getElementById(ID).innerHTML = INNER;
+}
+
+export function replaceATTRIB(ID, ATTRIB, VAL) {
+  document.getElementById(ID)[ATTRIB] = VAL;
+}
