3588db019ee02e9e7eab26c183ca9fba5d3ba81a
mimmeer <175632698+mimmeer-github@users.noreply.github.com>
2024-11-06 16:46:14 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-import { ver } from "main.js";
-import { decimalAdd, decimalSub, decimalMul, decimalDiv } from "numbers.js";
-import { replaceHTML, replaceATTRIB } from "dom.js";
+import { ver } from "/main.js";
+import { decimalAdd, decimalSub, decimalMul, decimalDiv } from "/numbers.js";
+import { replaceHTML, replaceATTRIB } from "/dom.js";
 console.log("JSX Version " + ver());
 alert(decimalAdd(1, 0.4));
 alert(decimalSub(0.3, 0.2));
