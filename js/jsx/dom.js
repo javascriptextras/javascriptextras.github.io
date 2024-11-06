14670e2ef5b16cb6260f29d4d2511db87a419dae
mimmeer <175632698+mimmeer-github@users.noreply.github.com>
2024-11-06 16:13:46 +0000

    Delete js directory

--- js/jsx/dom.js
+++
@@ -1,9 +0,0 @@
-type="module"
-
-export function replaceHTML(ID, INNER) {
-  document.getElementById(ID).innerHTML = INNER;
-}
-
-export function replaceATTRIB(ID, ATTRIB, VAL) {
-  document.getElementById(ID)[ATTRIB] = VAL;
-}
--- js/jsx/main.js
+++
@@ -1,5 +0,0 @@
-type="module"
-
-export function ver() {
-	return("JavaScript eXtras v1.0-alpha+1");
-}
