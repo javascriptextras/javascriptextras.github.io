1c2fff9fbb27b1f0be480f1ffc1067cb5a07755c
mimmeer <175632698+mimmeer-github@users.noreply.github.com>
2024-11-07 07:22:50 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,8 @@
-function buttonClick() {
-  addHTML("body", "<p>You may look at the code: <a href=\"https://github.com/javascriptextras/javascriptextras/blob/main/jsx/\">Github</a>");
-  createButton("Show Code", "buttonClick()", "button", "body");
-}
-
 import { ver } from "https://javascriptextras.github.io/javascriptextras/jsx/main.js";
 import { decimalAdd, decimalSub, decimalMul, decimalDiv } from "https://javascriptextras.github.io/javascriptextras/jsx/math.js";
 import { replaceHTML, replaceATTRIB, addHTML } from "https://javascriptextras.github.io/javascriptextras/jsx/dom.js";
 import { createButton, createLabel, createDiv } from "https://javascriptextras.github.io/javascriptextras/jsx/ui.js";
+import { move, rotate, hide, show, fadeIn, fadeOut, slideIn, slideOut } from "https://javascriptextras.github.io/javascriptextras/jsx/animate.js";
 console.log("JSX Version " + ver());
 alert(decimalAdd(1, 0.4));
 alert(decimalSub(0.3, 0.2));
@@ -16,3 +12,13 @@ replaceHTML("tst", "Yeah!");
 replaceATTRIB("tst", "style", "color: red");
 addHTML("body", '<p>Yeah</p>');
 createButton("Show Code", "buttonClick()", "button", "body");
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function buttonClick() {
+  hide("button");
+  addHTML("body", "<p>You may look at the code: <a href=\"https://github.com/javascriptextras/javascriptextras/blob/main/jsx/\">Github</a>");
+  createButton("Show Code", "buttonClick()", getRandomInt(999999), "body");
+}
